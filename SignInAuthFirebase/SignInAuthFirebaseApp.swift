c197b40ce6f2ea1a76e1d32bc1acebe7dcc32f70
NastasiaLarina <stucha84@mail.ru>
2021-05-17 15:22:46 +0500

    Initial Commit

---
+++ SignInAuthFirebase/SignInAuthFirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SignInAuthFirebaseApp.swift
+//  SignInAuthFirebase
+//
+//  Created by Анастасия Ларина on 17.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct SignInAuthFirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
